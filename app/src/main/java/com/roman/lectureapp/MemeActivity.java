7f89475690769866b7045fbefa57cff9ef62edf3
Roman Smirnov <roman.smirnv@gmail.com>
2017-12-18 14:31:41 +0200

    take camera pic

--- app/src/main/java/com/roman/lectureapp/MemeActivity.java
+++ app/src/main/java/com/roman/lectureapp/MemeActivity.java
@@ -1,9 +1,11 @@
 package com.roman.lectureapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,6 +30,8 @@ public class MemeActivity extends AppCompatActivity {
     TextView topText;
     String img_url = "https://i.imgflip.com/o5yxu.jpg";
     String MY_IMAGE = "my_image";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,61 +46,31 @@ public class MemeActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-//                set the text
                 topText.setText(editText.getText());
 //                make the text visible
                 topText.setVisibility(View.VISIBLE);
-
-//                get the image from disk if it already exists
-                if (isSaved()) {
-                    Picasso.with(MemeActivity.this)
-                            .load(MY_IMAGE)
-                            .into(imageView);
-                }else {
-//                load the image
-                    Picasso.with(MemeActivity.this)
-                            .load(img_url)
-                            .into(imageView, new Callback() {
-                                @Override
-                                public void onSuccess() {
-                //                get the bitmap
-                                    BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
-                                    Bitmap bitmap = drawable.getBitmap();
-                                    saveImage(bitmap);
-                                }
-
-                                @Override
-                                public void onError() {
-                                    Toast.makeText(MemeActivity.this, "failed to get image", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                }
+                dispatchTakePictureIntent();
             }
         });
     }
 
-    private boolean isSaved() {
-        File file = new File(MY_IMAGE);
-        return file.exists();
-    }
-
-    private void saveImage(Bitmap bmp) {
-        FileOutputStream out = null;
-        try {
-            out = new FileOutputStream(MY_IMAGE);
-            bmp.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
-            // PNG is a lossless format, the compression factor (100) is ignored
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (out != null) {
-                    out.close();
-                }
-            } catch (IOException e) {
-                System.out.println("not saved");
-            }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
 
 }
